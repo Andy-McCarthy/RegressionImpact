af5ba0b7db1eed1a85349bf1c4078dca351d7286
Andrew McCarthy <andrew.mccarthy@theperducogroup.com>
2017-06-12 13:31:51 -0400

    Made to look more like Cleveland Dot Plot

--- R/visualize.R
+++ R/visualize.R
@@ -1,13 +1,13 @@
 #' @title Visualize Regression Variables
 #'
-#' @description Function to visually display the significance of each predictor variable in
-#' a regression. The function takes an lm() object as input and outputs a ggplot2-labeled
-#' figure.
+#' @description Function to visually display the significance of each predictor
+#' variable in a regression. The function takes an lm() object as input and
+#' outputs a ggplot2-labeled figure.
 #'
 #' @param model an lm() object
 #' @param measure use P-values (P) or coefficients (C) to construct plot
 #' @param signif significance level for P-values
-#' @param sorted whether the variables should be plotted in order of significance
+#' @param sorted whether the variables should be ordered by significance
 #'
 #' @return a ggplot2 object
 #'
@@ -46,30 +46,19 @@ visualize <- function(model, measure = "P", signif = 0.05, sorted = TRUE) {
     for (i in 1:ncol(model$model)) {
       stdev[i] <- sd(model$model[,i])
     }
-    #
     imp <- coeff * stdev[2:model$rank]/stdev[1]
   } else {
-    message("For the measure argument, please enter 'P' for P-values or 'C' for")
-    message("Standardized Coefficients.")
+    message("For the measure argument, please enter 'P' for P-values or 'C'")
+    message("for Standardized Coefficients.")
     return()
   }
 
   # construct data frame
   pct <- data.frame(
     name = var.names,
-    impact = imp
+    impact = imp,
+    order = 1:(model$rank-1)
   )
-
-  # if the user chooses to have the variables sorted
-  if (sorted == T) {
-    # sort by impact
-    pct <- switch(measure,
-                  "P" = pct[order(pct$impact, decreasing = T),],
-                  "C" = pct[order(abs(pct$impact), decreasing = F),])
-  }
-
-  # add the order to the pct data frame
-  pct$order <- 1:(model$rank-1)
 
   # add the colors
   if (measure == "P") {
@@ -79,43 +68,70 @@ visualize <- function(model, measure = "P", signif = 0.05, sorted = TRUE) {
     }
   }
 
-  # determine longest variable name
-  str.l <- max(nchar(var.names))
-  # calculate X center for names
-  X.val <- -0.1 - 0.005*str.l
-
-  # construct variable names/locations data frame
-  var.labs <- data.frame(
-    name = pct$name,
-    X.coord <- rep(X.val,nrow(pct)),
-    Y.coord <- pct$order
-  )
-
   # plot results-----------------------------------------------------
+  if (sorted == T) {
+  # IF THE PLOT SHOULD BE SORTED
   gg.p <- switch(measure,
     #P-value------------
-    "P" = ggplot2::ggplot(data = var.labs, ggplot2::aes(x = X.coord, y = Y.coord)) +
-      ggplot2::geom_text(ggplot2::aes(label=var.labs$name, size = 14))  +
+    "P" = ggplot2::ggplot() +
+      # plot the actual P-values
+      ggplot2::geom_point(data = pct,
+                          ggplot2::aes(x = impact, y = reorder(name,-impact)),
+                          color = pct$sig) +
       # guidelines at 0, 1, and the significance level
       ggplot2::geom_vline(ggplot2::aes(xintercept = 0), color = "skyblue") +
       ggplot2::geom_vline(ggplot2::aes(xintercept = signif), color = "red") +
       ggplot2::geom_vline(ggplot2::aes(xintercept = 1), color = "skyblue") +
-      # plot the actual P-values
-      ggplot2::geom_point(data = pct, ggplot2::aes(x = impact, y = order),
-                          color = pct$sig, size = 2) +
       # plot aesthetics
-      ggplot2::coord_cartesian(xlim = c(-0.3,1.1)) +
-      ggplot2::scale_x_continuous(breaks = seq(from = -0.3, to = 1.1, by = 0.1),
-                                  labels = round(seq(from = -0.3, to = 1.1, by = 0.1),1)) +
-      ggplot2::scale_y_discrete(limits = c(0,model$rank)) +
+      ggplot2::scale_x_continuous(breaks = seq(from = 0, to = 1, by = 0.1),
+                          labels = round(seq(from = 0, to = 1, by = 0.1),1)) +
       ggplot2::xlab("P-value") + ggplot2::ylab("Regression Variables") +
-      ggplot2::theme(axis.text.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(size = 12)) +
-      ggplot2::theme(legend.position = "none"),
+      ggplot2::theme(axis.text.x = ggplot2::element_text(size = 12),
+                     axis.text.y = ggplot2::element_text(size = 12),
+                     legend.position = "none"),
 
     #Coefficients-------
-    "C" = ggplot2::ggplot(data = pct, ggplot2::aes(x = impact, y = order)) +
+    "C" = ggplot2::ggplot(data = pct,
+                   ggplot2::aes(x = impact, y = reorder(name,abs(impact)))) +
+      ggplot2::geom_text(ggplot2::aes(label=pct$name, size = 14)) +
+      # guidelines at -1, 0, and 1
+      ggplot2::geom_vline(ggplot2::aes(xintercept = 0), color = "red") +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = -1), color = "skyblue") +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = 1), color = "skyblue") +
+      # # other plot aesthetics
+      ggplot2::scale_x_continuous(limits = c(min(-1,-max(abs(pct$impact))-.1),
+                                             max(1,max(abs(pct$impact))+.1))) +
+      ggplot2::xlab("Standardized Coefficient") +
+      ggplot2::ylab("Regression Variables") +
+      ggplot2::theme(axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     axis.text.x = ggplot2::element_text(size = 12),
+                     legend.position = "none")
+  )
+  # IF THE PLOT SHOULD NOT BE SORTED
+  } else {
+    gg.p <- switch(measure,
+      #P-value------------
+      "P" = ggplot2::ggplot() +
+      # plot the actual P-values
+      ggplot2::geom_point(data = pct,
+                          ggplot2::aes(x = impact, y = reorder(name,-order)),
+                          color = pct$sig) +
+      # guidelines at 0, 1, and the significance level
+      ggplot2::geom_vline(ggplot2::aes(xintercept = 0), color = "skyblue") +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = signif), color = "red") +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = 1), color = "skyblue") +
+      # plot aesthetics
+      ggplot2::scale_x_continuous(breaks = seq(from = 0, to = 1, by = 0.1),
+                          labels = round(seq(from = 0, to = 1, by = 0.1),1)) +
+      ggplot2::xlab("P-value") + ggplot2::ylab("Regression Variables") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(size = 12),
+                     axis.text.y = ggplot2::element_text(size = 12),
+                     legend.position = "none"),
+
+      #Coefficients-------
+      "C" = ggplot2::ggplot(data = pct,
+                        ggplot2::aes(x = impact, y = reorder(name,-order))) +
       ggplot2::geom_text(ggplot2::aes(label=pct$name, size = 14)) +
       # guidelines at -1, 0, and 1
       ggplot2::geom_vline(ggplot2::aes(xintercept = 0), color = "red") +
@@ -124,13 +140,14 @@ visualize <- function(model, measure = "P", signif = 0.05, sorted = TRUE) {
       # other plot aesthetics
       ggplot2::scale_x_continuous(limits = c(min(-1,-max(abs(pct$impact))-.1),
                                              max(1,max(abs(pct$impact))+.1))) +
-      ggplot2::scale_y_discrete(limits = c(0,model$rank)) +
-      ggplot2::xlab("Standardized Coefficient") + ggplot2::ylab("Regression Variables") +
-      ggplot2::theme(axis.text.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(size = 12)) +
-      ggplot2::theme(legend.position = "none")
-  )
+      ggplot2::xlab("Standardized Coefficient") +
+      ggplot2::ylab("Regression Variables") +
+      ggplot2::theme(axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     axis.text.x = ggplot2::element_text(size = 12),
+                     legend.position = "none")
+    )
+  }
 
   # return plot
   return(gg.p)
